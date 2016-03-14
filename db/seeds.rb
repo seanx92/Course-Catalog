350fa755ca0caf4318997c854c70dc9e87ebc43a
seanx92 <dummyshooter@gmail.com>
2016-03-14 15:16:48 -0400

    Seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,32 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#records = JSON.parse(File.read('db/subject.json'))
-#records.each do |record|
-#  a = Subject.new
-#  a.identity = record['id']
-#  a.name = record['name']
-#  a.abbreviation = record['abbreviation']
-#  a.save
-#end
+records = JSON.parse(File.read('db/subject.json'))
+records.each do |record|
+  a = Subject.new
+  a.identity = record['id']
+  a.name = record['name']
+  a.abbreviation = record['abbreviation']
+  a.save
+end
+
+records = JSON.parse(File.read('db/course.json'))
+records.each do |record|
+  a = Course.new
+  a.code = record['code']
+  a.name = record['name']
+  a.credits = record['credits']
+  a.independent_study = record['independent_study']
+  a.save
+end
+
+records = JSON.parse(File.read('db/instructor.json'))
+records.each do |record|
+  a = Instructor.new
+  a.identity = record['id']
+  a.email = record['email']
+  a.first = record['first']
+  a.middle = record['middle']
+  a.last = record['last']
+  a.save
+end
