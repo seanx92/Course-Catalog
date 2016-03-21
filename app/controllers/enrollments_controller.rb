376079ec6b71ce762550db238d98f7bc00842b49
seanx92 <dummyshooter@gmail.com>
2016-03-21 16:55:46 -0400

    Bug fixed

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -70,6 +70,6 @@ class EnrollmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def enrollment_params
-      params.require(:enrollment).permit(:user_id, :course_id, :course_name)
+      params.permit(:user_id, :course_id, :course_name)
     end
 end
--- app/controllers/navigator_controller.rb
+++ app/controllers/navigator_controller.rb
@@ -2,6 +2,7 @@ class NavigatorController < ApplicationController
   before_action :require_login
 
   def index
+    @courses = Enrollment.where(user_id: current_user.id)
   end
 
 end
